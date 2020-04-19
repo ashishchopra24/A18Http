50ce5220d5e6e16c6e627b6a4954e0ada5d6fa7e
ashishchopra24 <49956840+ashishchopra24@users.noreply.github.com>
2020-04-19 17:51:09 +0530

    GetData

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Data} from './data.model';
 
 @Component({
   selector: 'app-root',
@@ -7,16 +9,18 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  loadedPosts = [];
+  loadedPosts: Data[] = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     this.http
       .post(
-        'https://postdata-65d56.firebaseio.com//posts.json',
+        'https://postdata-65d56.firebaseio.com/posts.json',
         postData
       )
       .subscribe(responseData => {
@@ -26,10 +30,37 @@ export class AppComponent implements OnInit{
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
+  }
+  onClearPosts() {
+    this.http.delete('https://postdata-65d56.firebaseio.com/posts.json').subscribe(() => {
+      this.loadedPosts = [];
+    });
+
   }
 
-  onClearPosts() {
-    // Send Http request
+  private fetchPosts() {
+    this.http
+      .get(
+        'https://postdata-65d56.firebaseio.com/posts.json'
+      )
+      .pipe(
+        map(responseData => {
+          const postsArray = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({...responseData[key]});
+            }
+          }
+          return postsArray;
+        })
+      )
+      .subscribe(posts => {
+        console.log(posts);
+        this.loadedPosts = posts;
+      });
+
   }
+
 
 }
